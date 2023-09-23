230142ff936566c5e1fa44ce9dfe0235cdc36dd1
Qi Peng <qi@canoo.com>
2023-09-23 14:04:36 -0700

    init

---
+++ model/m_random_forest.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+
+
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+
+
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
+clf.fit(X_train, y_train)
+
+
+
+y_pred = clf.predict(X_test)
+
+
+
+acc = accuracy_score(y_test, y_pred)
+print(f"Model Accuracy: {acc}")
---
+++ readme.md
@@ -0,0 +1,13 @@
+# 天啦噜
+
+
+## project env setup
+
+
+```bash
+mkdir .venv
+python3 -m venv .venv
+
+source .venv/bin/activate
+
+```
